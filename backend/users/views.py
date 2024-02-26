0e1bbd7d163aab394a5d5787e86242d747efe61d
Ilya <nikitinilya777@yandex.ru>
2024-02-26 21:30:14 +0700

    1

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,2 @@
 from django.shortcuts import render
 from rest_auth.registration.views import RegisterView
-from .models import User
-
-
-class CustomRegisterView(RegisterView):
-    queryset = User.objects.all()
